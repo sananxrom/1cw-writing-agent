414278e5bd79d5e9b94fb74046d321a65009c274
sananxrom <sanan@xrom.in>
2026-07-07 01:28:45 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '../styles/globals.css'
+import AuthGate from '../components/AuthGate'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthGate>
+      <Component {...pageProps} />
+    </AuthGate>
+  )
 }
